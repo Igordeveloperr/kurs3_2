b010fbf42af56dd61b79f93aa0b8b2a35cba0836
Igordeveloperr <g.bikmetow@gmail.com>
2025-03-02 12:23:22 +0300

    222

--- RPS/lab33/src/lab33/Main.java
+++ RPS/lab33/src/lab33/Main.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args) {
-    	// test comments 1 3232323232 3333333
+    	// test comments 1 3232323232
         BigFractionCalculatorGUI app = new BigFractionCalculatorGUI();
         app.setVisible(true);
     }
